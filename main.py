ee111841e4497fcc1ebd69b2a766e9e2ae5d244d
Arda Kara <ardakara97@yandex.com>
2019-01-03 10:42:15 +0300

    Implemented -PRINT, -VAR, -GOTO

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ class Narrator:
     def __init__(self):
         self.narrationLines = []
         self.cursorPos = 0
+        self.narrationValues = {}
 
         self.loadNarration()
         self.loadPreviousState()
 
     def loadNarration(self):
-        global narrator
-
         with open("data/narration.txt", encoding='utf-16') as narrationFile:
             
             startFound = False
@@ -28,8 +27,45 @@ class Narrator:
         #reload previous state of the narrative
 
     def narrate(self):
-        global end
-        end = True
+        self.cursorPos += 1
+
+        currentLine = self.narrationLines[self.cursorPos]
+
+        currentLine = currentLine.strip()
+
+        if currentLine.startswith("PRINT"):
+            currentLine = currentLine[5:]
+            currentLine = currentLine.strip()
+            print(currentLine)
+
+            return 2
+        elif currentLine.startswith("GOTO"):
+            currentLine = currentLine[4:]
+            currentLine = currentLine.strip()
+
+            self.cursorPos = 0
+        
+            for line in self.narrationLines:
+                if line.strip().startswith("<" + currentLine + ">"):
+                    break
+                else:
+                    self.cursorPos += 1
+
+            return 0
+
+        elif currentLine.startswith("VAR"):
+            currentLine = currentLine[3:]
+            currentLine = currentLine.strip()
+
+            equalityPos = currentLine.find("=")
+
+            name = currentLine[0:equalityPos].strip()
+            value = currentLine[equalityPos+1:].strip()
+
+            self.narrationValues[name] = value
+            print(self.narrationValues)
+            return 0
+
         return 0
         #move cursor to the next position and interpret
         #take necessary actions and save the game status
